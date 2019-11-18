2cf63f60069b3ab26464d277d788278ddb561cb7
Valdyn Hunt <valdyn@mac.com>
2019-11-18 09:48:05 -0800

    add test code

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,46 @@
-// Output Current Year
-$('.currentYear').text( (new Date).getFullYear() );
+$( document ).ready(function() {
+    // console.log('ok');
 
-/**********************************
-  CONFIGURE JSON CALL
-**********************************/ 
-// The API feed
-const url = 'https://gist.githubusercontent.com/valdynhunt/c5bb684b1cb356fa466a06e52cc5dc3f/raw/624255b598591b1c645fe6a3f2b095efda7fa12b/combined.json';
-// Do stuff with returned data
-let doStuff = function(data) {
- 
-  let q = document.getElementById('search').value;
-  document.getElementById('search').value = '';
+    // Store the canvas object into a variable
+    var $myCanvas = $('#myCanvas');
+
+    var img = document.getElementById("ceb");
+    $myCanvas.drawImage({
+        source: img,
+        x: 300, y: 300,
+        scale: 0.8
+    });
+
+    // rectangle shape 
+    $myCanvas.drawRect({
+        fillStyle: 'steelblue',
+        strokeStyle: 'steelblue',
+        strokeWidth: 1,
+        x: 150, y: 400,
+        fromCenter: false,
+        width: 20,
+        height: 10
+    });
+
+    $myCanvas.drawText({
+        text: '\uf0f4',
+        fontFamily: 'FontAwesome',
+        fontSize: 13,
+        x: 290, y: 450,
+        fillStyle: 'black',
+        strokeStyle: 'black',
+        strokeWidth: 1
+      });
+
+      $myCanvas.drawText({
+        text: '\uf2e7',
+        fontFamily: 'FontAwesome',
+        fontSize: 13,
+        x: 390, y: 400,
+        fillStyle: 'black',
+        strokeStyle: 'black',
+        strokeWidth: 1
+      });
+
   
-  var res = $(data.Search).filter(function (i, n){
-    return n.Title.includes(q) || n.Title.includes(q.toLowerCase()) || n.Title.includes(q.toUpperCase()) || n.Title.includes(toCapitalize(q)) || n.imdbID == q;
-  });
-
-  var arr=[];
-  temp=res.filter((x, i)=> {
-    if (arr.some(el => el.imdbID === i.imdbID)) {
-      return arr;
-    }
-    arr.push(i);
-  })
-  
-  const max = arr.length;
-
-  console.log(res.length);
-  console.log(max);
- 
-  for ( let ix = 0; ix < max; ix++ ) {
-    
-    let title = arr[ix].Title;
-    let year = arr[ix].Year;
-    let imdbLink = arr[ix].imdbID;
-    let img = arr[ix].Poster;
-  
-    const template = `
-        <section>  
-            <div class="movie-info">
-                <p class="title">${ title }</p>
-                <p class="year">${ year }</p>
-                <p class="imdb">
-                    <a href="http://imdb.com/title/${ imdbLink }" target="_blank">IMDB</a>
-                </p>
-            </div>
-            <img src="${ img }" alt="Poster of ${ title }" title="Poster of ${ title }"/>
-        </section>
-    `;  
-
-    $('main').append( template ); 
-
-  } 
-  
-};
-
-// Search button
-$('#submit').click(function(event){
-
-    $('main').empty();
-    event.preventDefault(); // cancel default behavior
-    $.getJSON( url, doStuff);
-
 });
-
-// Capitalize word(s) of search term
-const toCapitalize = (str)  => {
-    
-  if (str.split(" ").length - 1 == 0) {
-
-      return str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase(); 
-
-  } else {
-
-      let arr = str.split(" ");
-      let newStr = "";
-
-      for (let ix = 0; ix < arr.length; ix++) {
-
-          arr[ix] = arr[ix].substring(0,1).toUpperCase() + arr[ix].substring(1).toLowerCase();
-
-          newStr = newStr + arr[ix];
-
-          if (ix < arr.length - 1) {
-
-              newStr = newStr + " ";
-
-          }
-
-      }
-
-      return newStr;
-
-  }
-
-}
